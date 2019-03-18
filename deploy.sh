b9e0effdba40f62d47c95c72ca6d75c70cf77f0d
Bheemsh <bheemesh.baraguru@gmail.com>
2019-03-18 23:11:01 +0530

    Adding files ips.txt and deploy.txt

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ echo "Demonstrating the while reading"
 while read -r line  
 do
 	echo $line
-sudo sshpass -p "gamut" scp target/flipkart-1.0-SNAPSHOT.jar gamut@$line:/home/gamut/deploy/
+sshpass -p "gamut" scp target/flipkart-1.0-SNAPSHOT.jar gamut@$line:/home/gamut/deploy/
 done <"ips.txt"
 echo "deployement is done"
